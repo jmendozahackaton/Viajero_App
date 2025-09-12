3e63502c523616a70c35aa3da7bc0c1d62d764c8
Jorge Mendoza <jmendoza.hackaton@gmail.com>
2025-09-11 19:18:20 -0600

    Nuevos cambios en la implementación 11/09/25

---
+++ lib/core/network/firebase_service.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:hackaton_app/data/datasources/firebase_options.dart'; // Importa las opciones
+
+class FirebaseService {
+  static Future<void> initialize() async {
+    await Firebase.initializeApp(
+      options:
+          DefaultFirebaseOptions.currentPlatform, // Usa las opciones generadas
+    );
+
+    // Configurar mensajes en primer plano
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Mensaje en primer plano: ${message.notification?.title}');
+    });
+
+    // Solicitar permisos de notificación
+    await FirebaseMessaging.instance.requestPermission();
+  }
+
+  static FirebaseAuth get auth => FirebaseAuth.instance;
+  static FirebaseFirestore get firestore => FirebaseFirestore.instance;
+  static FirebaseStorage get storage => FirebaseStorage.instance;
+  static FirebaseMessaging get messaging => FirebaseMessaging.instance;
+}
